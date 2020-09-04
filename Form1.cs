7c67d08edffe3085f7f295dda4658a39adedef00
Paul Winter <zimin.pavel.v@gmail.com>
2020-09-04 14:37:39 +0300

    added actual length metrics

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,23 @@ namespace Converter
         {
             InitializeComponent();
             measure = new Dictionary<string, double>();
+
+            //  современные меры длины
+            double mkm = 1;
+            double mm = mkm * 1000;
+            double cm = mm * 10;
+            double inch = mm * 25.4;
+            double foot = inch * 12;
+            double yard = inch * 36;
+            double dm = mm * 100;
+            double m = mm * 1000;
+            double km = m * 1000;
+            double mile = yard * 1760;
+            double nautical_mile = m * 1852;
+
+            //  современные меры массы
+
+
         }
 
         private void MainForm_Load(object sender, EventArgs e)
